524632a587b356ba7ab3ad006487be25975e1048
wdavid <00135222@uca.edu.sv>
2024-08-14 18:04:37 -0600

    changues

--- app/metadata.js
+++ app/metadata.js
@@ -15,7 +15,7 @@ export const metadata = {
     siteName: 'Chiltiupán',
     images: [
       {
-        url: 'https://scontent.fsal2-1.fna.fbcdn.net/v/t39.30808-6/326264703_856586898785023_2067681554492837458_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=qBnw5Wef2xAQ7kNvgF19IEB&_nc_ht=scontent.fsal2-1.fna&oh=00_AYA_u2eOdmsyvNdBPhpgqLfeJ2zOQnzwwNmtL7A0svqopw&oe=66B97C30',
+        url: '/logoconocechiltiublack.png',
         width: 1200,
         height: 630,
         alt: 'Imagen de Chiltiupán',
